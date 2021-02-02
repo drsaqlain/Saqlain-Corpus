9c6d288655f58aeca540576b4ff7dbd16769b7ab
drsaqlain <78300370+drsaqlain@users.noreply.github.com>
2021-02-02 10:43:29 +0500

    Add files via upload

---
+++ tehran.py
@@ -0,0 +1,19 @@
+Iran rejects Bahrain blast accusation
+– Iran’s Deputy Foreign Minister for Arab and African Affairs Hossein Amir-Abdollahian has rejected claims by a Bahraini minister that Iran was involved in a recent deadly terrorist attack in the country.
+
+Amir-Abdollahian also condemned the July 28 bomb explosion in Bahrain’s eastern island of Sitra, situated 12 kilometers southeast of the capital, Manama, which killed two police officers. 
+
+He dismissed Bahraini Foreign Minister Sheikh Khaled bin Ahmed Al Khalifa’s claim about Tehran’s involvement in the bombing as totally “untrue.” 
+
+He said the accusation was deliberate as it was made to undermine the Iranian Foreign Minister Mohamed Javad Zarif’s recent tour of regional courtiers. 
+
+Zarif paid a three day visit to Qatar, Iraq and Kuwait this week. 
+
+“Unfortunately, the tribal behavior of some radical elements within the Bahraini government, and their deliberate insult to Muslims’ religious beliefs have caused a deep gap between the government and the people in Bahrain,” Amir-Abdollahian said. 
+
+He also called on the Bahraini foreign minister to play “a positive and constructive role regarding the collective security in the region and national reconciliation between the Bahraini government and its people instead of stoking religious strife in Bahrain and the region,” said. 
+
+
+The Iranian official added Iran only supports a political solution to the crisis Bahrain, stressing that Manama’s systematic human rights abuses and security crackdown will further complicate the situation in Bahrain. 
+
+Bahrain claimed on saturday that it had foiled an arms and explosives smuggling plot by two Bahrainis with ties to Iran. Later, Manama also claimed that the explosives used in the Sitra bombing were similar to those allegedly seized after being smuggled from Iran. 
